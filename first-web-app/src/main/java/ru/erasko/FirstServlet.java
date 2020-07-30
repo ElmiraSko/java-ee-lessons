36309bd296fdd23dd18aa8281a293c0f50d4cd3c
Elmira Skorobogatova <elmirasco@gmail.com>
2020-07-31 00:10:59 +0200

    Переделала модуль к уроку 1

---
+++ first-web-app/src/main/java/ru/erasko/FirstServlet.java
@@ -0,0 +1,35 @@
+package ru.erasko;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class FirstServlet implements Servlet {
+    private transient ServletConfig config;
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        this.config = servletConfig;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return this.config;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.setContentType("text/html");
+        servletResponse.getWriter().println("<h2>Hello from Servlet !!!</h2>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
+
